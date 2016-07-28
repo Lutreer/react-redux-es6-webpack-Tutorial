328a138c1d8151dd27b532cbc0904c83df165056
Leasong <lxuidesigner@gmail.com>
2016-07-28 21:23:53 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 var path = require('path');
 
 module.exports = {
-    entry: './app/index.js',
-    
+    entry: [
+        'webpack-dev-server/app?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        './app/index'
+    ],
+
     output: {
         path: path.join(__dirname, '/dist'),
         filename: 'bundle.js'
@@ -11,10 +15,27 @@ module.exports = {
     resolve: {
         extensions: ['', '.js', '.jsx']
     },
-    
+
     module: {
         loaders: [
-            { test: /\.js|jsx$/, loaders: ['babel'] }
+            {
+                test: /\.js|jsx$/,
+                include: [
+                    path.join(__dirname, "app"),
+                    path.join(__dirname, "components")
+                ],
+                loaders: ['react-hot', 'babel']
+
+
+            },
+            {
+                test: /\.css$/,
+                loader: "style!css"
+            },
+            {
+                test: /\.less/,
+                loader: 'style-loader!css-loader!less-loader'
+            }
         ]
     }
 }
