b51e9994d8af5be7b10e85ec5658a4d9ec20fa92
Leasong <lxuidesigner@gmail.com>
2016-10-18 20:06:54 +0800

    continue

--- app/redux/actions/todoAction.js
+++ app/redux/actions/todoAction.js
@@ -1,5 +1,18 @@
 import ACTION_TYPE from '../common/const_action_type'
 
 let actions = {
-
+    addTodo: function(text){
+        return {
+            type: ACTION_TYPE.TODO.ADD,
+            text: text
+        }
+    },
+    completeTodo: function(id){
+        return {
+            type: ACTION_TYPE.TODO.DELETE,
+            id: id
+        }
+    }
 }
+
+export default actions
--- app/redux/actions/userAction.js
+++ app/redux/actions/userAction.js
@@ -1,5 +1,12 @@
 import ACTION_TYPE from '../common/const_action_type'
 
 let actions = {
-
+    createNewUser: function(text){
+        return {
+            type: 'CREATE_USER_ID',
+            id: Math.round(Math.random() * 100)
+        }
+    }
 }
+
+export default actions
