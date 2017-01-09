cce3ea858f3d59e88aa92db929357b9f48d02eaa
leasong <lxuidesinger@gmail.com>
2017-01-10 03:05:36 +0800

    add saga to deal ajax

---
+++ app/redux/common/action_type/constActionType.js
@@ -0,0 +1,19 @@
+//统一管理 action type
+const ACTION_TYPE = {
+    TODO: {
+        ADD: 'ADD_TODO',
+        DELETE: 'DELETE_TODO',
+        UPDATE: 'UPDATE_TODO',
+        COMPLETE: 'COMPLETE_TODO',
+        SELECT: 'SELECT_ALL_TODOS'
+    },
+    USER: {
+        CREATE: 'CREATE_USER'
+    },
+    COMMON: {
+        FETCH_LOADING: 'FETCH_LOADING',
+        FETCH_SUCCEEDED: 'FETCH_SUCCEEDED',
+        FETCH_FAILED: 'FETCH_FAILED'
+    },
+};
+export default ACTION_TYPE
